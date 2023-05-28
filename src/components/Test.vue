66ebdc6917191206688af6294f9137c662f8c3a7
Leo(장석찬) <scjang@gabia.com>
2023-05-28 19:32:12 +0900

    feat: vue setting

---
+++ src/components/Test.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        Count: {{ count }}
+        <button @click="increment()">Increment</button>
+    </div>
+</template>
+
+<script lang="js">
+
+import { defineComponent, ref, onMounted } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const count = ref(0)
+
+    onMounted(() => {
+      count.value = 1
+    })
+
+    function increment() {
+      count.value += 1
+    }
+
+    return { count, increment }
+  },
+})
+</script>
